2bfa8ddf55bf44c4d9fab7bd5cea478b8bbb4f28
Bola <bolicd@gmail.com>
2018-03-17 20:27:16 +0100

    initial, problems with swagger

---
+++ WebApi2OwinBoilerplate/Controllers/TestController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Http;
+
+namespace WebApi2OwinBoilerplate.Controllers
+{
+    [RoutePrefix("api")]
+    public class TestController : ApiController
+    {
+        [Route("v1/test")]
+        public IHttpActionResult GetV1()
+        {
+            return Ok(new { value = "Hello" });
+        }
+
+        [Route("v2/test")]
+        public IHttpActionResult GetV2()
+        {
+            return Ok(new { value = "Hello V2" });
+        }
+    }
+}
---
+++ WebApi2OwinBoilerplate/Startup.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json.Serialization;
+using Owin;
+using StructureMap;
+using System;
+using System.Diagnostics;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Http;
+using System.Web.Routing;
+using WebApi2OwinBoilerplate.DependencyResolution;
+
+namespace WebApi2OwinBoilerplate
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder appBuilder)
+        {
+            HttpConfiguration config = WebApiConfiguration();
+            ConfigureStructureMap(config);
+            appBuilder.UseWebApi(config);
+        }
+
+        private static void ConfigureStructureMap(HttpConfiguration config)
+        {
+            //setting up IoC container - StructureMap
+            IContainer container = IoC.Initialize();
+            container.AssertConfigurationIsValid();
+            Debug.WriteLine(container.WhatDoIHave());
+            config.DependencyResolver = new StructureMapWebApiDependencyResolver(container);
+        }
+
+        private static HttpConfiguration WebApiConfiguration()
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            
+            // enable attribute routing
+            config.MapHttpAttributeRoutes();
+
+            //set default routes
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            //return camelcase json
+            var JsonFormater = config.Formatters.JsonFormatter;
+            JsonFormater.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
+            config.Formatters.JsonFormatter.SupportedMediaTypes
+                .Add(new MediaTypeHeaderValue("text/html"));
+
+
+            return config;
+        }
+    }
+
+    ////enforcing lowercase routes
+    public class LowercaseRouteConstraint : IRouteConstraint
+    {
+        public bool Match(HttpContextBase httpContext, Route route, string parameterName, RouteValueDictionary values, RouteDirection routeDirection)
+        {
+            var path = httpContext.Request.Url.AbsolutePath;
+            return path.Equals(path.ToLowerInvariant(), StringComparison.InvariantCulture);
+        }
+    }
+}
